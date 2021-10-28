f0e1cc51ebb0fd81d6b8b609220d1f5d35d1de13
Snork1715 <57507197+Snork1715@users.noreply.github.com>
2021-10-28 13:28:23 +0300

    second commit

--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export default function FriendsListItem(props) {
   const { id, avatar, name, active } = props;
   return (
     <li className="friend-item" id={id}>
-      <div className={(active) ? "item-div" : "item-div item-div__active"}></div>
+      <div className={active ? "item-div" : "item-div item-div__active"}></div>
       <img className="avatar" src={avatar} alt="" width="48" />
       <p className="name">{name}</p>
     </li>
@@ -12,8 +12,8 @@ export default function FriendsListItem(props) {
 }
 
 FriendsListItem.propTypes = {
-    id: PropTypes.number,
-    avatar: PropTypes.string,
-    name: PropTypes.string,
-    active: PropTypes.bool,
+  id: PropTypes.number,
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  active: PropTypes.bool,
 };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
 
-//const friend = friends[0];
-
 ReactDOM.render(
   <App />,
   document.querySelector("#root")
